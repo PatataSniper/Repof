356941ab7016b779d5e21f091fce364353f75358
jesuslirap <jesus.lirap@alumnos.udg.mx>
2020-04-09 09:04:54 -0500

    descarga

--- app/views/routes.py
+++ app/views/routes.py
@@ -1,9 +1,17 @@
 from app import app
+from flask import session, redirect
+
 from app.views.upload import *
-from flask import session
+from app.views.download import *
+
+HOME_ROUTE = app.config['HOME_ROUTE']
+
+@app.route('/')
+def __init__():
+    return redirect(HOME_ROUTE)
 
 @app.route('/home')
-def upload_form():
+def home():
     if 'data' in session and 'schema' in session:
         return render_template('home.html', data=session['data'], schema=session['schema'])
     return render_template('home.html')
